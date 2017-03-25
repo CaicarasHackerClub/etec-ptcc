26c4e0946fced179342384c856b000ee5f2c3708
giseleCarvalho <gih.carvalho.fernandes@gmail.com>
2017-03-25 12:27:59 -0300

    Pasta de arquivos de backend, atualmente Posto.class.php e verifica_usuario.php

---
+++ backend/Posto.class.php
@@ -0,0 +1,328 @@
+<?php
+Class Posto
+{
+	//pessoa
+	private $pes_nome;
+	private $pes_pai;
+	private $pes_mae;
+	private $pes_rg;
+	private $pes_cpf;
+	private $pes_data;
+	private	$pes_tipo;
+	private $pes_email;
+	private $pes_estado_civil;
+	private $pes_cidadania;
+	private $pes_genero;
+	private $pes_sexo_biologico;
+	private $pes_telefone;
+	
+	//Variaveis Endereço
+	private $end_pais;
+	private $end_estado;
+	private $end_cidade;
+	private $end_cep;
+	private $end_bairro;
+	private $end_destrito;
+	private $end_rua;
+	private $end_numero;
+	
+	//Funcionario
+	private $fun_cargo;
+	private $fun_setor;
+	private $fun_horario;
+	private $fun_inscricao;
+	
+	//usuario
+	private $usu_senha;
+	private $usu_email;
+	
+	//medico
+	private $med_especializacao;
+	private $med_crm;
+	
+	//paciente
+	private $pac_tipo_sangue;
+	private $pac_plano_saude;
+	private $pac_remedio;
+	private $pac_doenca;
+	private $pac_educacao;
+	private $pac_profissao;
+	
+	//enfermeiro
+	private $enf_registro;
+	
+	
+	//pessoa
+	function setPes_nome($nome){
+		$this->pes_nome = $nome;
+	}
+	function setPes_pai($pai){
+	  $this->pes_pai = $pai;
+	}
+	function setPes_mae($mae){
+	  $this->pes_mae = $mae;
+	}
+	function setPes_rg($rg){
+		$this->pes_rg = $rg;
+	}
+	function setPes_cpf($cpf){
+		$this->pes_cpf = $cpf;
+	} 
+	function setPes_data($data){ 
+		$this->pes_data  = $data;
+	}
+	function setPes_tipo($tipo){
+		$this->pes_tipo = $tipo;
+	}
+	function setPes_email($email){
+		$this->pes_email = $email;
+	}
+	function setPes_estado_civil($estado_civil){
+		$this->pes_estado_civil = $estado_civil;
+	}
+	function setPes_cidadania($cidadania){
+		$this->pes_cidadania = $cidadania;
+	}
+	function setPes_genero($genero){
+		$this->pes_genero = $genero;
+	}
+	function setPes_sexo_biologico($sexo_biologico){
+		$this->pes_sexo_biologico = $sexo_biologico;
+	}
+	function setPes_telefone($telefone){
+		$this->pes_telefone = $telefone;
+	}
+
+	//endereco
+	function setEnd_pais($pais){
+		$this->end_pais = $pais;
+	}
+	function setEnd_estado($estado){
+		$this->end_estado;
+	}
+	function setEnd_cidade($cidade){
+		$this->end_cidade = $cidade;
+	}
+	function setEnd_cep($cep){
+		$this->end_cep = $cep;
+	}
+	function setEnd_bairro($bairro){
+		$this->end_bairro = $bairro;
+	}
+	function setEnd_distrito($distrito){
+		$this->end_distrito = $distrito;
+	}
+	function setEnd_rua($rua){
+		$this->end_rua = $rua;
+	}
+	function setEnd_numero($numero){
+		$this->end_numero;
+	}
+		
+	//funcionario
+	function setFun_cargo($cargo){
+		$this->fun_cargo = $cargo;
+	} 
+	function setFun_horario($horario){
+		$this->fun_horario = $horario;
+	}
+	function setFun_inscricao($inscricao){
+		$this->fun_inscricao = $inscricao;
+	}
+	function setFun_turno($turno){
+		$this->fun_turno = $turno;
+	} 
+
+	//usuario
+	function setUsu_senha($senha){
+		$this->usu_senha = $senha;
+	}
+	function setUsu_email($email){
+		$this->usu_email = $email;
+	}
+
+	//medico
+	function setMed_turno($turno){
+		$this->med_turno = $turno;
+	}
+	function setMed_crm($crm){
+		$this->med_crm = $crm;
+	} 
+	//enfermeiro
+	function setEnf_registro($registro){
+		$this->enf_registro = $registro;
+	}
+	//paciente
+	function setPac_tipo_sangue($tipo_sangue){
+		$this->pac_tipo_sangue = $tipo_sangue;
+	}
+	function setPac_plano_saude($plano_saude){
+		$this->pac_plano_saude = $plano_saude;
+	}
+	function setPac_remedio($pac_remedio){
+		$this->pac_remedio = $pac_remedio;
+	}
+	function setPac_doenca($pac_doenca){
+		$this->pac_doenca = $pac_doenca;
+	}
+	function setPac_educacao($pac_educacao){
+		$this->pac_educacao = $pac_educacao;
+	}
+	function setPac_profissao($pac_profissao){
+		$this->pac_profissao = $pac_profissao;
+	}
+	//get pessoa 
+	function getPes_nome(){
+		return $this->pes_nome;
+	}
+	function getPes_pai(){
+		return $this->pes_pai;
+	}
+	function getPes_mae(){
+		return $this->pes_mae;
+	}
+	function getPes_rg(){
+		return $this->pes_rg;
+	} 
+	function getPes_cpf(){
+		return $this->pes_cpf;
+	} 
+	function getPes_data(){ 
+		return $this->pes_data;
+	}
+	function getPes_tipo(){
+		return $this->pes_tipo;
+	}
+	function getPes_email(){
+		return $this->pes_email;
+	}
+	function getPes_estado_civil(){
+		return $this->pes_estado_civil;
+	}
+	function getPes_cidadania(){
+		return $this->pes_cidadania;
+	}
+	function getPes_genero(){
+		return $this->pes_genero;
+	}
+	function getPes_sexo_biologico(){ 
+		return $this->pes_sexo_biologico;
+	} 
+	function getPes_telefone(){
+		return $this->pes_telefone;
+	}
+
+	//get endereco
+	function getEnd_pais(){
+		return $this->end_pais;
+	}
+	function getEnd_estado(){
+		return $this->end_estado;
+	}
+	function getEnd_cidade(){
+		return $this->end_cidade;
+	}
+	function getEnd_cep(){
+		return $this->end_cep;
+	}
+	function getEnd_bairro(){
+		return $this->end_bairro;
+	}
+	function getEnd_distrito(){
+		return $this->end_distrito;
+	}
+	function getEnd_rua(){
+		return $this->end_rua;
+	}
+	function getEnd_numero(){
+		return $this->end_numero;
+	}
+	//get funcionario
+	function getFun_cargo(){
+		return $this->fun_cargo;
+	} 
+	function getFun_horario(){
+		return $this->fun_horario;
+	}
+	function getFun_inscricao(){
+		return $this->fun_inscricao;
+	}
+	function getFun_turno(){
+		return $this->fun_turno;
+	} 
+			
+
+	//get senha
+	function getUsu_senha(){
+		return $this->usu_senha;
+	} 
+	function getUsu_email(){
+		return $this->usu_email;
+	} 
+
+	//get paciente	  
+	function getPac_tipo_sangue(){
+		return $this->pac_tipo_sangue;	
+	}
+	function getPac_plano_saude(){
+		return $this->pac_plano_saude;
+	}
+	function getPac_remedio(){
+		return $this->pac_remedio;
+	}
+	function getPac_doenca(){
+		return $this->pac_doenca;
+	}
+	function getPac_educacao(){
+		return $this->pac_educacao;
+	}
+	function getPac_profissao(){
+		return $this->pac_profissao;
+	}
+	//enfermeiro
+	function getEnf_registro(){
+		return $this->enf_registro;
+	}
+	
+	//Métodos gerais
+	function Inserir($ins){	
+		$con = $this->conectar();
+		mysqli_query($con,$ins) or die ("Erro no Inserir : " . mysqli_error ($con) . "<br>" . $ins);
+		
+		mysqli_close($con);
+		
+		return TRUE;
+	}
+		
+	
+	function selecionar($query){
+		$con = $this->conectar();
+		$res = mysqli_query($con, $query) or die ("Erro Selecionar()" . $query . mysqli_error($con));
+		$qtd = mysqli_num_rows($res);
+		if ($qtd == 0)
+			return $qtd;
+		else{
+			$posto = mysqli_fetch_array ($res);
+				
+			return $posto[0];             
+		}
+		mysqli_close($con);
+	}
+		
+	function conectar() {
+		error_reporting (E_ALL & ~ E_NOTICE & ~ E_DEPRECATED);
+		
+		$server  = "localhost";
+		$usuario = "root";
+		$senha   = "";
+		$banco   = "hospital";
+				
+		$con = mysqli_connect($server, $usuario, $senha, $banco) or die ("Erro 7: " . mysqli_connect_error());
+		$dbase = mysqli_select_db($con, $banco) or die ("Erro 8:" . mysqli_error($con));
+			
+		return $con;
+	}
+
+}
+	
+?>
---
+++ backend/verifica_usuario.php
@@ -0,0 +1,85 @@
+<?php
+	session_start();
+?>
+<html>
+	<head>
+	</head>
+	<body>
+		
+		<?php
+		//Verifica usuario e senha do index.php (login) 
+				
+		if (!isset ($_SESSION['tipo'])) {
+			if ($_POST['usu_email'] == "" || $_POST['usu_senha'] == "") {
+				//Direciona pro formulário de acesso
+				//echo "Digite os campos corretamente";
+				echo "<script>alert('Digite os campos corretamente')
+								location.href='index.php';</script>;";
+			}
+			else {
+				include_once ("Posto.class.php");
+				$posto = new Posto;
+				
+				$sel= "SELECT * FROM usuario WHERE usu_senha = '".$_POST['usu_senha'].
+						"' AND usu_email = '".$_POST['usu_email']."';";
+				$qtd = $posto->selecionar($sel);
+				
+				if ($qtd >=1){
+					$_SESSION['tipo'] = $qtd;
+					echo "Logado !!! <br>";
+					$log_data = date ("y-s-d");
+					$log_ip = $_SERVER['REMOTE_ADDR'];
+					$log_id = $_SESSION['tipo'];
+					$sel = "SELECT * FROM login_acesso WHERE
+								log_ip   = '" . $log_ip . "' AND
+								log_data = '" . $log_data . "' AND 
+								USUARIO_usu_id = "  . $_SESSION['tipo'] . "
+								;";
+					$ins = "INSERT INTO login_acesso (log_ip,log_data,USUARIO_usu_id) VALUES (
+								'" . $log_ip 					 . "',
+								'" . $log_data 				 . "',
+								 " . $_SESSION['tipo'] . "
+								);";
+					
+					$qtd = $posto->selecionar($sel);
+					if ($qtd >=1){
+						echo"Sessão cadastrada anteriormente!!";
+					}
+					else{
+						$ok = $posto->inserir($ins);
+					
+						if ($ok) {
+							echo "Cadastrado com sucesso!";
+						}
+						else {
+							echo "Sessão não cadastrada com sucesso!";
+						}
+						//Direciona pra página de usuário logado
+					}
+					echo 
+					"<script>
+						alert('Bem vindo!!');
+						location.href='../index.php';
+					</script>;";
+				}											
+				else {
+				//Direciona pro formulário de acesso
+					echo 
+					"<script>
+						alert('Usuario e/ou senha invalidos!!');
+						location.href='index.php';
+					</script>;";
+				}
+			}
+		}
+		else {
+			//Direciona pra página de usuário logado
+			echo 
+			"<script>
+				alert('Oláaa');
+				location.href='../index.php';
+			</script>;";
+		}
+		?>
+	</body>
+</html>
