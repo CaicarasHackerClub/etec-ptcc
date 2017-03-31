c2c0c287c9b1b39a1a80b313571fa507d31ff74a
Jonatas Ramos <jo962011@gmail.com>
2017-03-31 02:22:08 -0300

    Acrecentada a pasta backend o arquivo pagina_inicial.
    	Neste arquivo fica a página logo ápos o login.

---
+++ backend/pagina_inicial.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+  <head>
+
+    <meta name="viewport" content="width=device-width, user-scalable=no">
+    <meta charset="utf-8">
+    <title>Home</title>
+    <link rel="stylesheet" href="../css/main.css">
+    <script type="text/javascript" src="../js/script.js" ></script>
+  </head>
+  <body>
+
+    <div class="container">
+      <nav class="lateral" id="lateral">
+        <img class="logo" src="../img/logos/index.png">
+        <a href="#" id="user" class="nav_user">Usuário</a>
+        <a href="#" class="nav_user">Deslogar</a>
+        <div id="hora"></div>
+      </nav>
+
+      <div class="main">
+
+        <div class="cabecalho">
+          <header>
+            <img src="../img/icons/botao_menu.png" id="btn_slide" onclick="mostrarMenu()" >
+          </header>
+          <h1 id="titulo">Hug & Health</h1>
+        </div>
+
+        <div class="conteudoCentro">
+          <a class="anchor" href="#"><img class="avatars" src="../img/avatars/nurse.png"><p class="txt_avatar">Triagem</p></a>
+          <a class="anchor" href="#"><img class="avatars" src="../img/avatars/recepcao.png"><p class="txt_avatar">Recepção</p></a>
+          <a class="anchor" href="#"><img class="avatars" src="../img/avatars/adm.png"><p class="txt_avatar">Administração</p></a>
+        </div>
+
+      </div>
+    </div>
+  </body>
+</html>
