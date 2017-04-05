b2d8f65737f38c9e5e4ae5f527142a1a4c4c3264
Jonatas Ramos <jo962011@gmail.com>
2017-04-04 22:32:13 -0300

    Adicionado a pasta backend a pagina de sub-módulo.

---
+++ backend/sub_modulo.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+  <head>
+
+    <meta name="viewport" content="width=device-width, user-scalable=no">
+    <meta charset="utf-8">
+    <title>Home</title>
+    <link rel="stylesheet" href="../css/main.css">
+    <script type="text/javascript" src="../js/script.js" ></script>
+  </head>
+  <body>
+
+    <div class="container">
+      <nav class="lateral" id="lateral">
+        <img class="logo" src="../img/logos/index.png">
+        <main class="main main-lateral">
+          <a href="#" Class="anchor" id="modulo_1" onclick="mostrar()" >+ Módulo</a>
+          <ul class="submodulo" id="submodulo_1">
+            <li class="lista" ><a href="#" Class="anchor">Sub Módulo</a></li>
+            <li class="lista" ><a href="#" Class="anchor">Sub Módulo</a></li>
+            <li class="lista" ><a href="#" Class="anchor">Sub Módulo</a></li>
+            <li class="lista" ><a href="#" Class="anchor">Sub Módulo</a></li>
+          </ul>
+          <a href="#" Class="anchor" id="modulo_1" onclick="mostrar()" >+ Módulo</a>
+          <a href="#" Class="anchor" id="modulo_1" onclick="mostrar()" >+ Módulo</a>
+        </main>
+        <a href="#" id="user" class="nav_user">Usuário</a>
+        <a href="#" class="nav_user">Deslogar</a>
+        <div id="hora"></div>
+      </nav>
+
+      <div class="main">
+
+        <div class="cabecalho">
+          <header>
+            <img src="../img/icons/botao_menu.png" id="btn_slide" onclick="mostrarMenu()" >
+          </header>
+          <div class="config">
+            <a href="#" class="anchor"><img src="../img/icons/imprimir.png" class="image" alt="..."></a>
+          </div>
+          <h1 class="titulo tit-sub-modulo"><img src="" alt="imagem"></h1>
+        </div>
+
+        <div class="conteudoCentro centro-modulo">
+          <div class="main">
+            <main class="main main-centro">
+
+            </main>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </body>
+</html>
