1c750a7bba0d406706570b51803cca45ed51a2ea
Jonatas Ramos <jo962011@gmail.com>
2017-04-07 00:32:19 -0300

    FrontEnd: Alterações na pagina sub_modulo.pbp.
    	* Reposicionamento e ajustes nos conteudos
    	do cabeçalho para a responsividade.

--- backend/sub_modulo.php
+++ backend/sub_modulo.php
@@ -16,10 +16,10 @@
         <main class="main main-lateral">
           <a href="#" Class="anchor" id="modulo_1" onclick="mostrar()" >+ Módulo</a>
           <ul class="submodulo" id="submodulo_1">
-            <li class="lista" ><a href="#" Class="anchor a-link">Sub Módulo</a></li>
-            <li class="lista" ><a href="#" Class="anchor a-link">Sub Módulo</a></li>
-            <li class="lista" ><a href="#" Class="anchor a-link">Sub Módulo</a></li>
-            <li class="lista" ><a href="#" Class="anchor a-link">Sub Módulo</a></li>
+            <li class="lista" ><a href="#" Class="anchor ">Sub Módulo</a></li>
+            <li class="lista" ><a href="#" Class="anchor ">Sub Módulo</a></li>
+            <li class="lista" ><a href="#" Class="anchor ">Sub Módulo</a></li>
+            <li class="lista" ><a href="#" Class="anchor ">Sub Módulo</a></li>
           </ul>
           <a href="#" Class="anchor" id="modulo_2" >+ Módulo</a>
           <a href="#" Class="anchor" id="modulo_3" >+ Módulo</a>
@@ -37,15 +37,13 @@
             <img src="../img/icons/opcoes.png" id="btn_slide-down" onclick="mostrarConfig()" class="button">
           </header>
           <div class="config cabecalho-group" id="config">
-            <a href="#" class="anchor a-link"><img src="../img/icons/imprimir.png" class="image" alt="..."></a>
-            <a href="#" class="anchor a-link"><img src="../img/icons/editar.png" class="image" alt="..."></a>
-            <a href="#" class="anchor a-link"><img src="../img/icons/salvar.png" class="image" alt="..."></a>
+            <a href="#" class="anchor "><img src="../img/icons/imprimir.png" class="image" alt="..."></a>
+            <a href="#" class="anchor "><img src="../img/icons/editar.png" class="image" alt="..."></a>
+            <a href="#" class="anchor "><img src="../img/icons/salvar.png" class="image" alt="..."></a>
           </div>
           <div class="group-image cabecalho-group" >
-            <h1 class="tit-sub-modulo titulo">
-              <img class="img-titulo" src="../img/avatars/triagem.png" alt="">
-            </h1>
-            <label class="tit-sub-modulo titulo" id="lbl_triagem">Triagem</label>
+            <div class="tit-sub-modulo tit-image"></div>
+            <label class="tit-sub-modulo tit-text" id="lbl_triagem">Triagem</label>
           </div>
         </div>
 
