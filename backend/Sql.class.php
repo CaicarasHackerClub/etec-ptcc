aeea765ab32b7c65802e17458bdada6f5b094857
Lucas Xavier <lukasxavierleite@gmail.com>
2017-04-07 22:36:42 -0300

    Só alguns ajustes na organização do código

--- backend/Sql.class.php
+++ backend/Sql.class.php
@@ -6,14 +6,13 @@ class Sql {
     // $password = "helth";
     // $db = "helth_hospital";
       //
-  function conecta (){ 
-    include_once "conectar.php";
+  function conecta() {
+    include_once 'conectar.php';
     return $con;
   }
 
   function inserir($query) {
-    $con = conecta();
-
+    $con = $this->conecta();
     $res = mysqli_query($con, $query) or die("Erro: " . mysqli_error($con) . "<br> Query: " . $query);
 
     mysqli_close($con);
@@ -29,7 +28,6 @@ class Sql {
 
   function fetch($query) {
     $con = $this->conecta();
-
     $res = mysqli_query($con, $query) or die("Erro: " . mysqli_error($con) . "<br> Query: " . $query);
     $fetch = mysqli_fetch_array($res);
 
@@ -38,17 +36,20 @@ class Sql {
     return $fetch;
   }
 
-  function selecionar($query){
-    $con = conecta();
+  function selecionar($query) {
+    $con = $this->conecta();
     $res = mysqli_query($con, $query) or die ("Erro selecionar  r()" . $query . mysqli_error($con));
     $qtd = mysqli_num_rows($res);
-    if ($qtd == 0)
-      return $qtd;
-    else{
-      $posto = mysqli_fetch_array ($res);
 
+    if($qtd == 0) {
+      return $qtd;
+    }
+
+    else {
+      $posto = mysqli_fetch_array($res);
       return $posto[0];
     }
+
     mysqli_close($con);
   }
 }
