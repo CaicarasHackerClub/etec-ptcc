0a138d67c46a9fc1b5363ae75764990d42d5f1c5
Lucas Xavier <lukasxavierleite@gmail.com>
2017-04-11 20:38:04 -0300

    Arquivo de conexão padrão criado

---
+++ backend/conectar.php
@@ -0,0 +1,12 @@
+<?php
+    // $host = "192.168.0.170";
+    // $user = "helth";
+    // $password = "helth";
+    // $db = "helth_hospital";
+
+    $host = "localhost";
+    $user = "root";
+    $password = "root";
+    $db = "hospital1";
+    $con  = mysqli_connect($host, $user, $password, $db) or die ("Erro ao conectar: " . mysqli_connect_error());
+?>
