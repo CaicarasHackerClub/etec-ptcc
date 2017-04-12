80ce3d03e32a0928b68a911e1abcaf7839261fe5
Vagner Correa <vagner.correa.revocio@gmail.com>
2017-04-12 16:57:38 -0300

    *Foi colocado no database.txt os usuarios para entrar como 	administracao e recepcao, olhem lá para ver o e-mail e senha.
    	*Foi adicionado a tabela sub_setor e por enquanto retirado
    	o fk do campo usuario_usu_id da tabela funcionario, estava
    	dando erro ao inserir funcionario.Precisamos arrumar isso.
    
    	*Foram colocados como unicos os campos:
    	"pes_rg","pes_cpf" tabela pessoa.
    	"med_crm" tabela medico.
    	"fun_inscricao" tabela funcionario.
    	"enf_registro" tabela enfermeiro.
    	"usu_email" tabela usuario.
    	Para não aceitar dados iguais.

--- sql/helth_hospital.sql
+++ sql/helth_hospital.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Tempo de geração: 11/04/2017 às 18:54
+-- Tempo de geração: 12/04/2017 às 21:52
 -- Versão do servidor: 10.1.21-MariaDB
 -- Versão do PHP: 7.1.1
 
@@ -45,7 +45,10 @@ CREATE TABLE `endereco` (
 INSERT INTO `endereco` (`end_id`, `end_pais`, `end_estado`, `end_cidade`, `end_cep`, `end_bairro`, `end_rua`, `end_numero`, `pessoa_pes_id`) VALUES
 (1, 'Brasil', 'SP', 'Ubatuba', '11680-000', 'Centro', 'Central', 314, 1),
 (2, 'Brasil', 'SP', 'Ubatuba', '11680-000', 'Centro', 'Castro Alves', 507, 2),
-(3, 'Brasil', 'SP', 'Ubatuba', '11680-000', 'Centro', 'Taubaté', 314, 3);
+(3, 'Brasil', 'SP', 'Ubatuba', '11680-000', 'Centro', 'Taubaté', 314, 3),
+(4, 'Brasil', 'SP', 'Ubatuba', '11680-000', 'Ipiranguinha', 'Cascata', 288, 4),
+(5, 'Brasil', 'SP', 'Ubatuba', '11680-000', 'Estufa 2', 'Comercial', 212, 5),
+(6, 'Brasil', 'SP', 'Ubatuba', '11680-000', 'Sumaré', 'Longitude', 344, 6);
 
 -- --------------------------------------------------------
 
@@ -88,6 +91,17 @@ INSERT INTO `especializacao` (`esp_id`, `esp_nome`, `medico_med_id`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Estrutura para tabela `fila_de_espera`
+--
+
+CREATE TABLE `fila_de_espera` (
+  `fde_id` int(11) NOT NULL,
+  `hora` time DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- --------------------------------------------------------
+
+--
 -- Estrutura para tabela `funcionario`
 --
 
@@ -107,7 +121,10 @@ CREATE TABLE `funcionario` (
 
 INSERT INTO `funcionario` (`fun_id`, `fun_cargo`, `fun_horario`, `fun_inscricao`, `fun_turno`, `usuario_usu_id`, `pessoa_pes_id`) VALUES
 (1, 'medico', '08:00:00', 1111111, 'noturno', 1, 1),
-(2, 'enfermeiro', '08:00:00', 1111112, 'noturno', 1, 2);
+(2, 'enfermeiro', '08:00:00', 1111112, 'noturno', 1, 2),
+(3, 'administracao', '07:00:00', 1222222, 'noturno', 3, 4),
+(4, 'recepcao', '07:00:00', 13333333, 'noturno', 4, 5),
+(5, 'recpcionista', '07:00:00', 1444444, 'noturno', 5, 6);
 
 -- --------------------------------------------------------
 
@@ -201,7 +218,10 @@ CREATE TABLE `pessoa` (
 INSERT INTO `pessoa` (`pes_id`, `pes_nome`, `pes_pai`, `pes_mae`, `pes_rg`, `pes_cpf`, `pes_data`, `pes_tipo`, `pes_email`, `pes_estado_civil`, `pes_cidadania`, `pes_genero`, `pes_sexo_biologico`, `pes_telefone`) VALUES
 (1, 'Karina Ramos', 'Mathos Ramos', 'Larisa Matos', '43.434.434-3', '346.346.346-34', '2005-04-21', 1, 'karina@bol.com.br', 'Solteira', 'Ubatubense', 'Feminino', 'Feminino', '(12)982343434'),
 (2, 'Marta wender', 'Joao da Silva', 'Jorge Wender', '366.457.987.98', '445.346.987.65', '2003-08-01', 2, 'martinhawender@email.com.br', 'Casada', 'Paulista', 'Masculino', 'Feminino', '(12)981236789'),
-(3, 'Paula dos Santos', 'Roberto Nascimento', 'Larisa Pereira', '33.050.250-3', '123.456.789-10', '1996-12-06', 3, 'Paulinha@gmail.com', 'Solteira', 'Paraense', 'Feminino', 'Feminino', '(35)997667890');
+(3, 'Paula dos Santos', 'Roberto Nascimento', 'Larisa Pereira', '33.050.250-3', '123.456.789-10', '1996-12-06', 3, 'Paulinha@gmail.com', 'Solteira', 'Paraense', 'Feminino', 'Feminino', '(35)997667890'),
+(4, 'Kleber Ramos', 'Jose Ramos', 'Pata Mathos', '909.876.897.76', '763.765.899.09', '0000-00-00', 1, 'klberzinho@email.com', 'Casado', 'Ubatubense', 'Masculino', 'Masculino', '(12)987667894'),
+(5, 'luiza Fonseca', 'Pedro Fonseca', 'Maria dias Fonseca', '333.654.765.98', '786.567.823.76', '1984-08-01', 1, 'Luizinha@hot.com', 'Solteira', 'Ubatubense', 'Feminino', 'Feminino', '(12)982456674'),
+(6, 'Rafael Nunes', 'Mario da Cruz Nunes', 'Joana lopes Nunes', '771.123.678.90', '456.765.912.56', '1997-06-25', 1, 'rafanudes@gmail.com', 'Solteiro', 'Ubatubense', 'masculino', 'masculino', '(12)38765590');
 
 -- --------------------------------------------------------
 
@@ -241,6 +261,18 @@ CREATE TABLE `setor` (
   `set_descricao` varchar(255) COLLATE utf8_bin NOT NULL,
   `set_responsavel` varchar(60) COLLATE utf8_bin NOT NULL,
   `funcionario_fun_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `sub_setor`
+--
+
+CREATE TABLE `sub_setor` (
+  `sbs_id` int(11) NOT NULL,
+  `sbs_nome` varchar(45) COLLATE utf8_bin DEFAULT NULL,
+  `setor_set_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- --------------------------------------------------------
@@ -288,7 +320,10 @@ CREATE TABLE `usuario` (
 
 INSERT INTO `usuario` (`usu_id`, `usu_nome`, `usu_senha`, `usu_email`, `usu_ativo`, `usu_tipo`) VALUES
 (1, 'Gisele', '123', 'gih@gih', 1, 1),
-(2, 'Admin', 'admin', 'admin@admin', 1, 1);
+(2, 'Admin', 'admin', 'admin@admin', 1, 1),
+(3, 'Kleber', '123', 'etec1@etec', 1, 1),
+(4, 'Luiza', '123', 'etec2@etec', 1, 1),
+(5, 'Rafael', '123', 'etec3@etec', 1, 1);
 
 --
 -- Índices de tabelas apagadas
@@ -306,6 +341,7 @@ ALTER TABLE `endereco`
 --
 ALTER TABLE `enfermeiro`
   ADD PRIMARY KEY (`enf_id`),
+  ADD UNIQUE KEY `enf_registro` (`enf_registro`),
   ADD KEY `fk_enfermeiro_funcionario1_idx` (`funcionario_fun_id`);
 
 --
@@ -316,10 +352,17 @@ ALTER TABLE `especializacao`
   ADD KEY `fk_especializacao_medico1_idx` (`medico_med_id`);
 
 --
+-- Índices de tabela `fila_de_espera`
+--
+ALTER TABLE `fila_de_espera`
+  ADD PRIMARY KEY (`fde_id`);
+
+--
 -- Índices de tabela `funcionario`
 --
 ALTER TABLE `funcionario`
   ADD PRIMARY KEY (`fun_id`),
+  ADD UNIQUE KEY `fun_inscricao` (`fun_inscricao`),
   ADD KEY `fk_FUNCIONARIOS_USUARIO1_idx` (`usuario_usu_id`),
   ADD KEY `fk_funcionario_pessoa1_idx` (`pessoa_pes_id`);
 
@@ -335,6 +378,7 @@ ALTER TABLE `login_acesso`
 --
 ALTER TABLE `medico`
   ADD PRIMARY KEY (`med_id`),
+  ADD UNIQUE KEY `med_crm` (`med_crm`),
   ADD KEY `fk_medico_funcionario1_idx` (`funcionario_fun_id`);
 
 --
@@ -348,13 +392,17 @@ ALTER TABLE `paciente`
 -- Índices de tabela `pessoa`
 --
 ALTER TABLE `pessoa`
-  ADD PRIMARY KEY (`pes_id`);
+  ADD PRIMARY KEY (`pes_id`),
+  ADD UNIQUE KEY `pes_cpf` (`pes_cpf`),
+  ADD UNIQUE KEY `pes_rg` (`pes_rg`);
 
 --
 -- Índices de tabela `plano_de_saude`
 --
 ALTER TABLE `plano_de_saude`
   ADD PRIMARY KEY (`pds_id`),
+  ADD UNIQUE KEY `pds_num_convenio` (`pds_num_convenio`),
+  ADD UNIQUE KEY `pds_numero_sus` (`pds_numero_sus`),
   ADD KEY `fk_plano_de_saude_paciente1_idx` (`pac_id`);
 
 --
@@ -371,6 +419,13 @@ ALTER TABLE `setor`
   ADD KEY `fk_setor_funcionario1_idx` (`funcionario_fun_id`);
 
 --
+-- Índices de tabela `sub_setor`
+--
+ALTER TABLE `sub_setor`
+  ADD PRIMARY KEY (`sbs_id`),
+  ADD KEY `fk_sub_setor_setor1_idx` (`setor_set_id`);
+
+--
 -- Índices de tabela `triagem`
 --
 ALTER TABLE `triagem`
@@ -381,7 +436,8 @@ ALTER TABLE `triagem`
 -- Índices de tabela `usuario`
 --
 ALTER TABLE `usuario`
-  ADD PRIMARY KEY (`usu_id`);
+  ADD PRIMARY KEY (`usu_id`),
+  ADD UNIQUE KEY `usu_email` (`usu_email`);
 
 --
 -- AUTO_INCREMENT de tabelas apagadas
@@ -391,7 +447,7 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT de tabela `endereco`
 --
 ALTER TABLE `endereco`
-  MODIFY `end_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `end_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 --
 -- AUTO_INCREMENT de tabela `enfermeiro`
 --
@@ -403,10 +459,15 @@ ALTER TABLE `enfermeiro`
 ALTER TABLE `especializacao`
   MODIFY `esp_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
+-- AUTO_INCREMENT de tabela `fila_de_espera`
+--
+ALTER TABLE `fila_de_espera`
+  MODIFY `fde_id` int(11) NOT NULL AUTO_INCREMENT;
+--
 -- AUTO_INCREMENT de tabela `funcionario`
 --
 ALTER TABLE `funcionario`
-  MODIFY `fun_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `fun_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 --
 -- AUTO_INCREMENT de tabela `login_acesso`
 --
@@ -426,7 +487,7 @@ ALTER TABLE `paciente`
 -- AUTO_INCREMENT de tabela `pessoa`
 --
 ALTER TABLE `pessoa`
-  MODIFY `pes_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `pes_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 --
 -- AUTO_INCREMENT de tabela `plano_de_saude`
 --
@@ -451,7 +512,7 @@ ALTER TABLE `triagem`
 -- AUTO_INCREMENT de tabela `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `usu_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `usu_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 --
 -- Restrições para dumps de tabelas
 --
@@ -478,7 +539,6 @@ ALTER TABLE `especializacao`
 -- Restrições para tabelas `funcionario`
 --
 ALTER TABLE `funcionario`
-  ADD CONSTRAINT `fk_FUNCIONARIOS_USUARIO1` FOREIGN KEY (`usuario_usu_id`) REFERENCES `usuario` (`usu_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `fk_funcionario_pessoa1` FOREIGN KEY (`pessoa_pes_id`) REFERENCES `pessoa` (`pes_id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
@@ -510,6 +570,12 @@ ALTER TABLE `plano_de_saude`
 --
 ALTER TABLE `setor`
   ADD CONSTRAINT `fk_setor_funcionario1` FOREIGN KEY (`funcionario_fun_id`) REFERENCES `funcionario` (`fun_id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Restrições para tabelas `sub_setor`
+--
+ALTER TABLE `sub_setor`
+  ADD CONSTRAINT `fk_sub_setor_setor1` FOREIGN KEY (`setor_set_id`) REFERENCES `setor` (`set_id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
 -- Restrições para tabelas `triagem`
