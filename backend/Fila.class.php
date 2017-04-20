ef745da68fceaa7876e97a7acae46b7016ebf4c7
Lucas Xavier <lukasxavierleite@gmail.com>
2017-04-20 20:23:43 -0300

    Fila de espera como tabela. Algumas alterações na classe

--- backend/Fila.class.php
+++ backend/Fila.class.php
@@ -3,8 +3,60 @@
 include_once 'Sql.class.php';
 
 class Fila extends Sql {
+  private $emConsulta;
+  private $emEspera;
 
-  function espera($dataPac, $horaPac) {
+  private $espera;
+  private $nome;
+  private $id;
+  private $tempoAtual;
+
+  private $cor;
+  private $tempo = array(10, 60, 120, 240);
+
+  private $tabela =
+  "
+  <table border='1'>
+  <thead>
+    <th> # </th>
+    <th> Nome </th>
+    <th> Tempo de espera </th>
+  </thead>";
+
+
+  function getTempo() {
+    return $this->tempo;
+  }
+
+  function getCor() {
+    return $this->cor;
+  }
+
+  function __construct() {
+    $this->atualizar(0, 0, -1);
+    $this->cor[0] = "SELECT * FROM triagem WHERE tri_classe_risco = 'Laranja' AND tri_status = 'Em espera';";
+    $this->cor[1] = "SELECT * FROM triagem WHERE tri_classe_risco = 'Amarelo' AND tri_status = 'Em espera';";
+    $this->cor[2] = "SELECT * FROM triagem WHERE tri_classe_risco = 'Verde' AND tri_status = 'Em espera'";;
+    $this->cor[3] = "SELECT * FROM triagem WHERE tri_classe_risco = 'Azul' AND tri_status = 'Em espera';";
+  }
+
+  function setEspera($espera) {
+    $this->espera = $espera;
+  }
+
+  function getEspera() {
+    return $this->espera;
+  }
+
+  function getEmEspera() {
+    return $this->emEspera;
+  }
+
+  function getEmConsulta() {
+    return $this->emConsulta;
+  }
+
+  function calc($dataPac, $horaPac) {
     $data = new DateTime($dataPac);
     $horario = new DateTime($horaPac);
 
@@ -22,12 +74,45 @@ class Fila extends Sql {
     return $tempoFinal;
   }
 
-  function chamar($id) {
-    $con = parent::conecta();
+  function chamar() {
+    if($this->id != 0) {
 
-    $up = "UPDATE triagem SET tri_status = 'Em consulta' WHERE tri_id = " . $id . ";";
-    $res = mysqli_query($con, $up) or die("Erro: " . mysqli_error($con) . "<br> Query: " . $up);
+    parent::inserir("UPDATE triagem SET tri_status = 'Em consulta' WHERE tri_id = " . $this->id . ";");
 
+    echo "Chamada: $this->nome, ID:  $this->id, tempo de espera: $this->espera <br>";
+  }
+  }
+
+  function cat() {
+    $this->tabela .=
+    "<tr>
+      <td>" . $this->id . "</td>
+      <td>" . $this->nome . "</td>
+      <td>" . $this->espera . "/" . $this->tempoAtual . " minutos </td>
+    </tr>";
+  }
+
+  function atualizar($pes, $id, $i) {
+    $this->emConsulta = parent::num("SELECT tri_id FROM triagem WHERE tri_status = 'Em consulta';");
+    $this->emEspera = parent::num("SELECT tri_id FROM triagem WHERE tri_status = 'Em espera'");
+
+    if($pes != 0) {
+      $pesId = parent::selecionar("SELECT pessoa_pes_id FROM paciente WHERE pac_id = " . $pes . ";");
+      $this->nome = parent::selecionar("SELECT pes_nome FROM pessoa WHERE pes_id = " . $pesId . ";");
+    }
+    if($id != 0) {
+      $this->id = $id;
+    }
+
+    if($i != -1) {
+      $this->tempoAtual = $tempo[$i];
+    }
+  }
+
+  function imprimir() {
+    $tabela .= "</table>";
+
+    echo $this->tabela;
   }
 }
 
--- backend/fila.php
+++ backend/fila.php
@@ -5,20 +5,14 @@
   $sql = new Sql();
   $fila = new Fila();
 
-  // $cor[0] = "SELECT * FROM triagem WHERE tri_classe_risco = 'Vermelho' AND tri_status = 'Em espera'";
-  $cor[0] = "SELECT * FROM triagem WHERE tri_classe_risco = 'Laranja' AND tri_status = 'Em espera';";
-  $cor[1] = "SELECT * FROM triagem WHERE tri_classe_risco = 'Amarelo' AND tri_status = 'Em espera';";
-  $cor[2] = "SELECT * FROM triagem WHERE tri_classe_risco = 'Verde' AND tri_status = 'Em espera'";;
-  $cor[3] = "SELECT * FROM triagem WHERE tri_classe_risco = 'Azul' AND tri_status = 'Em espera';";
-
-  $tempo = array(10, 60, 120, 240);
-
   $pacMax = 5;
 
-  $emConsulta = $sql->num("SELECT tri_id FROM triagem WHERE tri_status = 'Em consulta';");
-  $emEspera = $sql->num("SELECT tri_id FROM triagem WHERE tri_status = 'Em espera'");
+  $cor = $fila->getCor();
+  $tempo = $fila->getTempo();
 
   $con = $sql->conecta();
+
+  $fila->chamar(52);
 
   for($i = 0; $i < count($cor); $i++) {
     $res = mysqli_query($con, $cor[$i]);
@@ -26,25 +20,24 @@
     while($pac = mysqli_fetch_array($res)) {
       $data = $pac['tri_data'];
       $hora = $pac['tri_hora'];
-      $espera = $fila->espera($data, $hora);
+      $fila->setEspera($fila->calc($data, $hora));
 
-      $pesId = $sql->selecionar("SELECT pessoa_pes_id FROM paciente WHERE pac_id = " . $pac['id_paciente'] . ";");
-      $nome = $sql->selecionar("SELECT pes_nome FROM pessoa WHERE pes_id = " . $pesId . ";");
+      $fila->atualizar($pac['id_paciente'], $pac['tri_id'], $i);
 
-      $emConsulta = $sql->num("SELECT tri_id FROM triagem WHERE tri_status = 'Em consulta';");
-      $emEspera = $sql->num("SELECT tri_id FROM triagem WHERE tri_status = 'Em espera'");
-
-      if($emConsulta < $pacMax || $espera >= $tempo[$i]) {
-        $fila->chamar($pac['tri_id']);
-        echo "Chamada: $nome, ID: " . $pac['tri_id'] . ", tempo de espera: $espera . <br>";
+      if($fila->getEmConsulta() < $pacMax || $fila->getEspera() >= $tempo[$i]) {
+        $fila->chamar();
       }
-      else {
-        echo "Na fila de espera:  $nome, ID: " . $pac['tri_id'] . ", tempo de espera:  $espera/$tempo[$i] <br>";
 
+      else {
+        $fila->cat();
       }
     }
   }
 
-  echo "Pessoas em consulta: $emConsulta <br>";
-  echo "Pessoas em espera: $emEspera <br>";
+  mysqli_close($con);
+
+  echo "Pessoas em consulta: " . $fila->getEmConsulta() . "<br>";
+  echo "Pessoas em espera: " . $fila->getEmEspera() . "<br>";
+
+  $fila->imprimir();
 ?>
