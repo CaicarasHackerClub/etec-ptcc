2885d2d19e3be327fcbbcb50ea77eb39f4acd50f
Lucas Xavier <lukasxavierleite@gmail.com>
2017-04-22 18:59:37 -0300

    Classificação como chave estrangeira das cores da nova tabela.

--- backend/triagem.php
+++ backend/triagem.php
@@ -72,68 +72,68 @@ if (!isset($_POST['Recepcao']) && !isset($_POST['classificar'])) {
   <?php
 
 } else {
-    include_once 'Sql.class.php';
-    include_once 'Triagem.class.php';
+  include_once 'Sql.class.php';
+  include_once 'Triagem.class.php';
 
-    $sql = new Sql();
-    $tri = new Triagem();
+  $sql = new Sql();
+  $tri = new Triagem();
 
-    $id = isset($_POST['id']) && !empty($_POST['id']) ? $_POST['id'] : "0";
-    $temp = isset($_POST['temp']) && !empty($_POST['temp']) ? trim($_POST['temp']) : "0"; //
-    $pas = isset($_POST['pas']) && !empty($_POST['pas']) ? trim($_POST['pas']) : "0"; //
-    $pad = isset($_POST['pad']) && !empty($_POST['pad']) ? trim($_POST['pad']) : "0"; //
-    $peso = isset($_POST['peso']) && !empty($_POST['peso']) ? trim($_POST['peso']) : "0";
-    $altura = isset($_POST['altura']) && !empty($_POST['altura']) ? trim($_POST['altura']) : "0";
-    $batimento = isset($_POST['batimento']) && !empty($_POST['batimento']) ? trim($_POST['batimento']) : "0"; //
-    $oxi = isset($_POST['oxi']) && !empty($_POST['oxi']) ? trim($_POST['oxi']) : "0";
-    $resp = isset($_POST['resp']) && !empty($_POST['resp']) ? trim($_POST['resp']) : "0"; //
-    $dor = isset($_POST['dor']) && !empty($_POST['dor']) ? trim($_POST['dor']) : "0";
-    $orgaos = isset($_POST['org']) ? 1 : 0;
-    $data = date('Y-m-d');
-    $hora = date('H:i');
+  $id = isset($_POST['id']) && !empty($_POST['id']) ? $_POST['id'] : 0;
+  $temp = isset($_POST['temp']) && !empty($_POST['temp']) ? trim($_POST['temp']) : 0; //
+  $pas = isset($_POST['pas']) && !empty($_POST['pas']) ? trim($_POST['pas']) : 0; //
+  $pad = isset($_POST['pad']) && !empty($_POST['pad']) ? trim($_POST['pad']) : 0; //
+  $peso = isset($_POST['peso']) && !empty($_POST['peso']) ? trim($_POST['peso']) : 0;
+  $altura = isset($_POST['altura']) && !empty($_POST['altura']) ? trim($_POST['altura']) : 0;
+  $batimento = isset($_POST['batimento']) && !empty($_POST['batimento']) ? trim($_POST['batimento']) : 0; //
+  $oxi = isset($_POST['oxi']) && !empty($_POST['oxi']) ? trim($_POST['oxi']) : 0;
+  $resp = isset($_POST['resp']) && !empty($_POST['resp']) ? trim($_POST['resp']) : 0; //
+  $dor = isset($_POST['dor']) && !empty($_POST['dor']) ? trim($_POST['dor']) : 0;
+  $orgaos = isset($_POST['org']) ? 1 : 0;
+  $data = date('Y-m-d');
+  $hora = date('H:i');
 
-    // $temp = $sql->verifica($_POST['temp']);
-    // $pas  = $sql->verifica($_POST['pas']);
-    // $pad  = $sql->verifica($_POST['pad']);
-    // $batimento  = $sql->verifica($_POST['batimento']);
-    // $resp = $sql->verifica($_POST['resp']);
+  // $temp = $sql->verifica($_POST['temp']);
+  // $pas  = $sql->verifica($_POST['pas']);
+  // $pad  = $sql->verifica($_POST['pad']);
+  // $batimento  = $sql->verifica($_POST['batimento']);
+  // $resp = $sql->verifica($_POST['resp']);
 
-    $class = isset($_POST['class']) ? $_POST['class'] : "Indefinido";
+  $class = isset($_POST['class']) ? $_POST['class'] : "0";
 
-    $tri->setPacId($id);
-    $tri->setTemp($temp);
-    $tri->setPas($pas);
-    $tri->setPad($pad);
-    $tri->setPeso($peso);
-    $tri->setAltura($altura);
-    $tri->setBatimento($batimento);
-    $tri->setOxi($oxi);
-    $tri->setClass($class);
-    $tri->setResp($resp);
-    $tri->setDor($dor);
-    $tri->setOrg($orgaos);
-    $tri->setData($data);
-    $tri->setHora($hora);
+  $tri->setPacId($id);
+  $tri->setTemp($temp);
+  $tri->setPas($pas);
+  $tri->setPad($pad);
+  $tri->setPeso($peso);
+  $tri->setAltura($altura);
+  $tri->setBatimento($batimento);
+  $tri->setOxi($oxi);
+  $tri->setClass($class);
+  $tri->setResp($resp);
+  $tri->setDor($dor);
+  $tri->setOrg($orgaos);
+  $tri->setData($data);
+  $tri->setHora($hora);
 
-    // Se o usuário já tiver clicado no botão "Classificar" ou "Aceitar cor"
-    if (isset($_POST['classificar'])) {
-      $status = $tri->getClass() == "Vermelho" ? "Em consulta" : "Em espera";
-      $tri->setStatus($status);
+  // Se o usuário já tiver clicado no botão "Classificar" ou "Aceitar cor"
+  if (isset($_POST['classificar'])) {
+    $status = $tri->getClass() == 5 ? "Em consulta" : "Em espera";
+    $tri->setStatus($status);
 
-      $query = "INSERT INTO `triagem`(tri_temperatura, tri_pressao, tri_peso, tri_altura,
-          tri_batimento, tri_oxigenacao, tri_classe_risco, tri_respiracao, tri_dor,
-          tri_orgaos_vitais, tri_data, tri_hora, tri_status, id_paciente) VALUES("
-          . $tri->getTemp() . ", '"  . $tri->getPas()    . "x"   . $tri->getPad()     . "', "
-          . $tri->getPeso() . ", "   . $tri->getAltura() . ", "  . $tri->getBatimento() . ", "
-          . $tri->getOxi()  . ", '"  . $tri->getClass()  . "', " . $tri->getResp()    . ", "
-          . $tri->getDor()  . ", "   . $tri->getOrg()    . ", '" . $tri->getData()    . "', '"
-          . $tri->getHora() . "', '" . $tri->getStatus() . "', " . $tri->getPacId()   . ");";
+    $query = "INSERT INTO `triagem`(tri_temperatura, tri_pressao, tri_peso, tri_altura,
+        tri_batimento, tri_oxigenacao, tri_classe_risco, tri_respiracao, tri_dor,
+        tri_orgaos_vitais, tri_data, tri_hora, tri_status, id_paciente) VALUES("
+        . $tri->getTemp() . ", '"  . $tri->getPas()    . "x"   . $tri->getPad()       . "', "
+        . $tri->getPeso() . ", "   . $tri->getAltura() . ", "  . $tri->getBatimento() . ", "
+        . $tri->getOxi()  . ", "   . $tri->getClass()   . ", " . $tri->getResp()      . ", "
+        . $tri->getDor()  . ", "   . $tri->getOrg()    . ", '" . $tri->getData()      . "', '"
+        . $tri->getHora() . "', '" . $tri->getStatus() . "', " . $tri->getPacId()     . ");";
 
-      if ($sql->inserir($query)) {
-        echo "Inserido com sucesso";
-      } else {
-        echo "Erro ao inserir";
-      }
+    if ($sql->inserir($query)) {
+      echo "Inserido com sucesso";
+    } else {
+      echo "Erro ao inserir";
+    }
   } else {
     // Pegando a idade do paciente
     $fetchId = $sql->fetch("SELECT pessoa_pes_id FROM paciente WHERE pac_id = " . $tri->getPacId() . ";");
@@ -159,25 +159,25 @@ if (!isset($_POST['Recepcao']) && !isset($_POST['classificar'])) {
 
     // Classificando
     if (($tri->getResp() < 10 || $tri->getResp() > 30) && $tri->getResp() != 0) {
-      $tri->setClass("Vermelho");
+      $tri->setClass(5);
     } else {
       if (($tri->getPas() < 80 && $tri->getPad() < 50) && ($tri->getResp() != 0 && $tri->getPas() != 0 && $tri->getPad() != 0)) {
-        $tri->setClass("Laranja");
+        $tri->setClass(4);
       } else {
         if ($tri->getPas() >= 220 && $tri->getPad() >= 120 && $tri->getOrg() == 1) {
-          $tri->setClass("Laranja");
+          $tri->setClass(4);
         } else {
           if ($tri->getTemp() >= 39 && $tri->getTemp() != 0) {
-            $tri->setClass("Amarelo");
+            $tri->setClass(3);
           } else {
             if ($tri->getPas() >= 220 && $tri->getPad() >= 120 && $tri->getOrg() == 0) {
-              $tri->setClass("Amarelo");
+              $tri->setClass(3);
             } else {
               if ($idade >= 60 || $tri->getPas() >= 180 && $tri->getPas() < 220 && $tri->getPad() >= 120 && $tri->getPad() < 130 && $tri->getOrg == 0) {
-                $tri->setClass("Verde");
+                $tri->setClass(2);
                 echo "Idade do paciente: " . $idade;
               } else {
-                $tri->setClass("Azul");
+                $tri->setClass(1);
               }
             }
           }
@@ -187,27 +187,27 @@ if (!isset($_POST['Recepcao']) && !isset($_POST['classificar'])) {
 
     <form class="form form-classi" action="triagem.php" method="post">
     <h1 class="titulo"> Classificação: </h1>
-    <input type="radio" id="vermelho" class="inp_class" name="class" value="Vermelho" required <?php if ($tri->getClass() == "Vermelho") {
+    <input type="radio" id="vermelho" class="inp_class" name="class" value="5" required <?php if ($tri->getClass() == 5) {
       echo "checked";
     } ?> >
     <label for="vermelho" class="lbl-radio-class lbl_class"><p>Vermelho</p></label><br>
 
-    <input type="radio" id="laranja" class="inp_class" name="class" value="Laranja" required <?php if ($tri->getClass() == "Laranja") {
+    <input type="radio" id="laranja" class="inp_class" name="class" value="4" required <?php if ($tri->getClass() == 4) {
       echo "checked";
     } ?> >
     <label for="laranja" class="lbl-radio-class lbl_class"><p>Laranja</p></label><br>
 
-    <input type="radio" id="amarelo" class="inp_class" name="class" value="Amarelo" required <?php if ($tri->getClass() == "Amarelo") {
+    <input type="radio" id="amarelo" class="inp_class" name="class" value="3" required <?php if ($tri->getClass() == 3) {
       echo "checked";
     } ?> >
     <label for="amarelo" class="lbl-radio-class lbl_class"><p>Amarelo</p></label><br>
 
-    <input type="radio" id="verde" class="inp_class" name="class" value="Verde" required <?php if ($tri->getClass() == "Verde") {
+    <input type="radio" id="verde" class="inp_class" name="class" value="2" required <?php if ($tri->getClass() == 2) {
       echo "checked";
     } ?> >
     <label for="verde" class="lbl-radio-class lbl_class"><p>Verde</p></label><br>
 
-    <input type="radio" id="azul" class="inp_class" name="class" value="Azul" required <?php if ($tri->getClass() == "Azul") {
+    <input type="radio" id="azul" class="inp_class" name="class" value="1" required <?php if ($tri->getClass() == 1) {
       echo "checked";
     } ?> >
     <label for="azul" class="lbl-radio-class lbl_class"><p>Azul</p></label>
