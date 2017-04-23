bb63248621bdd8d2206e0c25002365f1bbe8d063
Alejandro Druetta <aledruetta@gmail.com>
2017-04-22 23:17:06 -0300

    Infra: Tirando php-cs-fixer do fixer.sh
        O comando `php-cs-fixer` tenta resolver os problemas detectados
        pelo `gulp back`. O problema é que ele está atrelado aos
        padrões PSR2 e estes impom indentação 4 espaços.
        Não existe uma forma simples de contornar esse problema.
        Por enquanto, então, o `fixer.sh` vai solucionar apenas:
        - Trainling whitespaces
        - End of lines
        Tudo além, precisa ser fixado manualmente.
    
    	modified:   fixer.sh

--- fixer.sh
+++ fixer.sh
@@ -1,27 +1,40 @@
 #!/bin/bash
 
+set -eux
+
 D2U=$(which dos2unix)
 PCF=$(which php-cs-fixer)
 
 if [[ ! -x "$D2U" ]] ; then
-    sudo apt -y update && sudo apt -y install dos2unix
+  sudo apt -y update && sudo apt -y install dos2unix
 fi
 
 if [[ ! -x "$PCF" ]] ; then
-    wget http://cs.sensiolabs.org/download/php-cs-fixer-v2.phar -O php-cs-fixer
-    sudo chmod a+x php-cs-fixer
-    sudo mv php-cs-fixer /usr/local/bin/php-cs-fixer
+  wget http://cs.sensiolabs.org/download/php-cs-fixer-v2.phar -O php-cs-fixer
+  sudo chmod a+x php-cs-fixer
+  sudo mv php-cs-fixer /usr/local/bin/php-cs-fixer
 fi
+
+# Transformar tabs em 2 espaços
+# find . -type f -name "*.php" | awk '{print "expand -t 2", $0, "> tmp ; mv tmp", $0}' | sh
+
+# Nova versão de transformar tabs em 2 espaços
+FILES=$(find . -type f -name "*.php")
+
+for f in $FILES; do
+  echo "$f"
+  expand -t 4 "$f" > tmp
+  mv tmp "$f"
+done
 
 # Unificar o caractere EOL pro formato Unix '\n'
 find . -type f -name "*.php" -exec dos2unix {} \;
 
-# Transformar tabs em 4 espaços
-find . -type f -name "*.php" | awk '{print "expand -t 2 ", $0, " > tmp; mv tmp ", $0}' | sh
-
 # Eliminar trailing whitespaces
 find . -type f -name "*.php" -exec sed -i -E 's/[[:space:]]+$//' {} \;
 
-# Tentar resolver alguns erros com o php-cs-fixer
-find . -type f -name "*.php" -exec php-cs-fixer fix {} --rules='{"braces": {"position_after_functions_and_oop_constructs": "same"}}' \;
-
+# TODO Problema com indentação a 2 espaços (phpcbf)
+# php-cs-fixer
+# find . -type f -name "*.php" -exec php-cs-fixer fix {} \
+#   --rules="@PSR2" \
+#   --rules='{"braces": {"position_after_functions_and_oop_constructs": "same"}}' \;
