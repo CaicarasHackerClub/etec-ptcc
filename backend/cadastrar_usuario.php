d78dd4a5199d41fbb46f66098b3f1b4fb9778052
Alejandro Druetta <aledruetta@gmail.com>
2017-04-26 16:28:57 -0300

    Corrigindo estilo em cadastrar_usuario.php

--- backend/cadastrar_usuario.php
+++ backend/cadastrar_usuario.php
@@ -1,27 +1,27 @@
 <html>
-    <head>
-    </head>
-    <body>
-        <?php
-        //cadastro de usuario e senha
+  <head>
+  </head>
+  <body>
+    <?php
+    //cadastro de usuario e senha
 
-        include_once ("Posto.class.php");
-        $posto = new Posto;
+    include_once("Posto.class.php");
+    $posto = new Posto;
 
-        $posto->setUsu_email($_POST['Usu_email']);
-        $posto->setUsu_senha($_POST['Usu_senha']);
+    $posto->setUsu_email($_POST['Usu_email']);
+    $posto->setUsu_senha($_POST['Usu_senha']);
 
-          $sel = "SELECT * FROM usuario WHERE usu_email ='" . $_POST['usu_email'] . "' ;";
-          $ins = "INSERT INTO usuario (usu_email,usu_senha) VALUES (
-                  '" . $posto->getUsu_email() . "'
-                  '" . $posto->getUsu_senha() . "'
-          );";
-          $ok = $posto->inserir($sel,$ins);
-            if ($ok) {
-              echo "Usuario cadastrado";
-            } else {
-              echo "Email j? existe!";
-            }
+      $sel = "SELECT * FROM usuario WHERE usu_email ='" . $_POST['usu_email'] . "' ;";
+      $ins = "INSERT INTO usuario (usu_email,usu_senha) VALUES (
+          '" . $posto->getUsu_email() . "'
+          '" . $posto->getUsu_senha() . "'
+      );";
+      $ok = $posto->inserir($sel, $ins);
+        if ($ok) {
+          echo "Usuario cadastrado";
+        } else {
+          echo "Email j? existe!";
+        }
 
 
 
