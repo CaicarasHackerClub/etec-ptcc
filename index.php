036ab911573ca89a07b2280533d2e0d9d53e8cde
Alejandro Druetta <aledruetta@gmail.com>
2017-04-26 16:24:50 -0300

    Corrigindo estilo em index.php

--- index.php
+++ index.php
@@ -8,29 +8,28 @@ session_start();
   </head>
   <body>
     <?php
-    if (!isset ($_SESSION ['id_usu'])) {
-      if (!isset ($_POST['usuario']) && !isset($_POST['senha'])) {
-        ?>
-      <img src="img/logos/index.png" class="Img">
-          <form class="FormLogin" action="backend/verifica_usuario.php" method="post">
-            <input type="text" name="usu_email" size="28" class="inp_class" placeholder="Usuario"><br>
-            <input type="password" name="usu_senha" size="28" class="inp_class" placeholder="Senha"><br>
-            <input type="submit" name="subm" value="Login" class="submit">
-          </form>
+    if (!isset($_SESSION['id_usu'])) {
+      if (!isset($_POST['usuario']) && !isset($_POST['senha'])) {
+      ?>
+        <img src="img/logos/index.png" class="Img">
+        <form class="FormLogin" action="backend/verifica_usuario.php" method="post">
+          <input type="text" name="usu_email" size="28" class="inp_class" placeholder="Usuario"><br>
+          <input type="password" name="usu_senha" size="28" class="inp_class" placeholder="Senha"><br>
+          <input type="submit" name="subm" value="Login" class="submit">
+        </form>
       <?php
-
       }
     } else {
-      $acao = isset($_GET['acao'])? $_GET['acao'] : ""; ?>
+      $acao = isset($_GET['acao']) ? $_GET['acao'] : ""; ?>
       <a href="?acao=cadastro">Cadastro</a>
       <a href="?acao=logoff">Sair</a>
       <?php
       if ($acao == "cadastro") {
-        header ("Location:backend/cadastro.php");
+        header("Location:backend/cadastro.php");
       } else {
         if ($acao == "logoff") {
           session_destroy();
-          unset ($_SESSION['tipo']);
+          unset($_SESSION['tipo']);
           echo "<script>alert('Tchau!! Volte sempre!!')
         location.href='index.php';</script>;";
         }
