4a2952a522331a057983822b1a80789478eef3e2
Alejandro Druetta <aledruetta@gmail.com>
2017-04-30 19:40:02 -0300

    Carregando a API do Maps de forma asíncrona

--- js/geo.js
+++ js/geo.js
@@ -1,37 +1,6 @@
+var map;
+
 $(function() {
-  var santaCasa = {lat: -23.4350898, lng: -45.0714174};
-
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 15,
-    center: santaCasa
-  });
-
-  var markerSC = new google.maps.Marker({
-    title: 'Santa Casa',
-    map: map,
-    animation: google.maps.Animation.DROP,
-    position: santaCasa
-  });
-
-  var infoSC = '<div class="poi-info-window gm-style">' +
-      '<div class="address">' +
-        '<div class="title full-width">Santa Casa de Ubatuba</div>' +
-        '<div class="address-line full-width">R. Conceição, 135 - Centro, Ubatuba - SP, 11680-000, Brazil</div>' +
-        '<div class="address-line full-width">+55 12 3834-3230</div>' +
-        '<div class="view-link"><a href="http://santacasaubatuba.org.br">santacasaubatuba.org.br</a></div>' +
-      '</div>' +
-    '</div>';
-
-  var infoWindowSC = new google.maps.InfoWindow({
-    content: infoSC
-  });
-
-  markerSC.addListener('click', function() {
-    infoWindowSC.open(map, markerSC);
-  });
-
-  infoWindowSC.open(map, markerSC);
-
   $('.search-form').submit(function(ev) {
     ev.preventDefault();
     var pessoa = $('.search-in').val();
@@ -63,3 +32,38 @@ $(function() {
       });
   });
 });
+
+function initMap() {
+  var santaCasa = {lat: -23.4350898, lng: -45.0714174};
+
+  map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 15,
+    center: santaCasa
+  });
+
+  var markerSC = new google.maps.Marker({
+    title: 'Santa Casa',
+    map: map,
+    animation: google.maps.Animation.DROP,
+    position: santaCasa
+  });
+
+  var infoSC = '<div class="poi-info-window gm-style">' +
+      '<div class="address">' +
+        '<div class="title full-width">Santa Casa de Ubatuba</div>' +
+        '<div class="address-line full-width">R. Conceição, 135 - Centro, Ubatuba - SP, 11680-000, Brazil</div>' +
+        '<div class="address-line full-width">+55 12 3834-3230</div>' +
+        '<div class="view-link"><a href="http://santacasaubatuba.org.br">santacasaubatuba.org.br</a></div>' +
+      '</div>' +
+    '</div>';
+
+  var infoWindowSC = new google.maps.InfoWindow({
+    content: infoSC
+  });
+
+  markerSC.addListener('click', function() {
+    infoWindowSC.open(map, markerSC);
+  });
+
+  infoWindowSC.open(map, markerSC);
+}
