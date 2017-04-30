c0f7186469aa9da6e1fe731fdc3269f54263e441
Alejandro Druetta <aledruetta@gmail.com>
2017-04-29 23:25:31 -0300

    Movendo form search event listener dentro de initMap

--- backend/geolocalizar.php
+++ backend/geolocalizar.php
@@ -1,20 +1,24 @@
 <?php
-header("content-type:application/json");
+  header("content-type:application/json");
 
-include_once 'Sql.class.php';
+  include_once 'Sql.class.php';
 
-$pessoa = isset($_GET['pessoa']) ? $_GET['pessoa'] : null;
+  $pessoa = isset($_GET['pessoa']) ? $_GET['pessoa'] : null;
 
-$sql = new Sql;
-$con = $sql->conecta();
+  $mysqli = new Sql;
+  $con = $mysqli->conecta();
 
-$query = "SELECT e.end_rua, e.end_numero, e.end_cidade FROM endereco e INNER JOIN pessoa p ON e.pessoa_pes_id = p.pes_id WHERE p.pes_nome = '$pessoa';";
+  $query = "SELECT e.end_rua, e.end_numero, e.end_cidade "
+         . "FROM endereco e "
+         . "INNER JOIN pessoa p "
+         . "ON e.pessoa_pes_id = p.pes_id "
+         . "WHERE p.pes_nome = '$pessoa';";
 
-$res = mysqli_query($con, $query) or die("Erro: id paciente " . mysqli_error($con) . "<br> Query: " . $query);
-$arrEndereco = mysqli_fetch_assoc($res);
+  $res = mysqli_query($con, $query) or die("Erro: " . mysqli_error($con) . ", Query: $query");
+  $arrEndereco = mysqli_fetch_assoc($res);
 
-mysqli_close($con);
+  mysqli_close($con);
 
-echo json_encode($arrEndereco);
+  echo json_encode($arrEndereco);
 
-exit();
+  exit();
