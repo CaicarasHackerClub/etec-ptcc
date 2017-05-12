69e3b4873009a17f5de2b4b3b3a70dcbfc50cdd6
giseleCarvalho <gih.carvalho.fernandes@gmail.com>
2017-05-12 19:35:58 -0300

    Backend: Verificando os campos
    	- Se for confirmação final ou update o campo estará desabilitado
    	e com a value correspondente.

--- backend/form_paciente.php
+++ backend/form_paciente.php
@@ -18,33 +18,86 @@ $paciente = $sql->fetch($selPac);
   <div class="group-form group-form-cadastro">
     <label class="lbl_class">Tipo Sanguineo</label>
       <?php
-      $sql->selectbox("tipo_sanguineo");
+      if ($_SESSION['form'] == 1){
+        $sql->selectbox("tipo_sanguineo");
+      } else {
+        echo "<input class=\"inp_class\" type=\"text\" name=\"pac_tipo_sanguineo\" size=\"28\" disabled value = " . $paciente[1] . "><br>";
+      }
       ?>
   </div>
   <div class="group-form group-form-cadastro">
     <label class="lbl_class">Remedio</label>
+      <?php
+      if ($_SESSION['form'] == 1) {
+        $dis = "";
+        $val = "";
+      } else {
+        $dis = " disabled";
+        $val = " value=\"" . $paciente[2] . "\"";
+      }
+      ?>
     <input class="inp_class" type="text" name="pac_remedio" size="28"><br>
   </div>
   <div class="group-form group-form-cadastro">
     <label class="lbl_class">Doença</label>
+    <?php
+      if ($_SESSION['form'] == 1) {
+        $dis = "";
+        $val = "";
+      } else {
+        $dis = " disabled";
+        $val = " value=\"" . $paciente[2] . "\"";
+      }
+    ?>
     <input class="inp_class" type="text" name="pac_doenca" size="28"><br>
   </div>
   <div class="group-form group-form-cadastro">
     <label class="lbl_class">Grau de escolaridade:</label>
     <?php
-    $sql->selectbox("escolaridade");
+    if ($_SESSION['form'] == 1){
+      $sql->selectbox("escolaridade");
+    } else {
+      echo "<input class=\"inp_class\" type=\"text\" name=\"pac_escolaridade\" size=\"28\" disabled value = " . $paciente[3] . "><br>";
+    }
     ?>
   </div>
   <div class="group-form group-form-cadastro">
     <label class="lbl_class">Convênio:</label>
+    <?php
+      if ($_SESSION['form'] == 1) {
+        $dis = "";
+        $val = "";
+      } else {
+        $dis = " disabled";
+        $val = " value=\"" . $paciente[4] . "\"";
+      }
+    ?>
     <input class="inp_class" type="text" name="pds_convenio_nome" size="28"><br>
   </div>
   <div class="group-form group-form-cadastro">
     <label class="lbl_class">Número :</label>
+    <?php
+      if ($_SESSION['form'] == 1) {
+        $dis = "";
+        $val = "";
+      } else {
+        $dis = " disabled";
+        $val = " value=\"" . $paciente[5] . "\"";
+      }
+    ?>
     <input class="inp_class" type="text" name="pds_num_convenio" size="28"><br>
   </div>
   <div class="group-form group-form-cadastro">
     <label class="lbl_class">SUS:</label>
+    <?php
+    if ($_SESSION['form'] == 1) {
+      $dis = "";
+      $val = "";
+    } else {
+      $dis = " disabled";
+      $val = " value=\"" . $paciente[2] . "\"";
+    }
+    ?>
     <input class="inp_class" type="text" name="pds_numero_sus" size="28">
   </div>
   <input class="inp_class submmit" type="submit" value="Confirmar">
