e4df588ed0fa4651b3c823d9ba26e248f5e37f28
giseleCarvalho <gih.carvalho.fernandes@gmail.com>
2017-05-12 22:26:29 -0300

    Backend: Verificações e instanciação
    	- Se o campo de texto for confirmacao final ou update
    	os campos estarão desabilitados e com value. Ja o cadastro
    	será apenas o campo de texto.
    	- Inserindo variaveis $val e $dis nos campos de texto
    	- Instanciando Sql.class.php

--- backend/form_funcionario.php
+++ backend/form_funcionario.php
@@ -1,4 +1,7 @@
 <?php
+include_once("Sql.class.php");
+$sql = New Sql;
+
 if ($_SESSION['form'] == 1) {
   $tipo = "cadastro.php?acao=cadastro&passo=3";
 } elseif ($_SESSION['form'] == 2) {
@@ -19,43 +22,104 @@ $usuario = $sql->fetch($selUsu);
   <h1 class="titulo">Funcionário</h1>
   <div class="group-form group-form-cadastro">
     <label class="lbl_class">Cargo:</label>
-    <select class="select" name="fun_cargo">
-      <option class="option" value="recepcao">Recepcionista</option>
-      <option class="option" value="medico">Médico</option>
-      <option class="option" value="enfermeiro">Enfermeiro</option>
-      <option class="option" value="funcionario">Funcionário</option>
-    </select>
+    <?php
+    if ($_SESSION['form'] == 1){
+    echo "<select class=\"select\" name=\"fun_cargo\">";
+    echo  "<option class=\"option\" value=\"recepcao\">Recepcionista</option>";
+    echo  "<option class=\"option\" value=\"medico\">Médico</option>";
+    echo  "<option class=\"option\" value=\"enfermeiro\">Enfermeiro</option>";
+    echo  "<option class=\"option\" value=\"funcionario\">Funcionário</option>";
+    echo "</select>";
+
+    } else {
+      echo "<input class=\"inp_class\" type=\"text\" name=\"fun_cargo\" size=\"28\"
+            disabled value = " . $funcionario[1] . "><br>";
+    }
+    ?>
     <br>
   </div>
   <div class="group-form group-form-cadastro">
     <label class="lbl_class">Setor:</label>
     <?php
-    $sql->selectbox("setor");
+    if ($_SESSION['form'] == 1) {
+      $sql->selectbox("setor");
+    } else {
+      echo "<input class=\"inp_class\" type=\"text\" name=\"fun_setor\" size=\"28\"
+            disabled value = " . $funcionario[2] . "><br>";
+    }
     ?>
   </div>
   <div class="group-form group-form-cadastro">
     <label class="lbl_class">Horario:</label>
-    <input class="inp_class" type="time" name="fun_horario" size="28"><br>
+    <?php
+      if ($_SESSION['form'] == 1) {
+        $dis = "";
+        $val = "";
+      } else {
+        $dis = " disabled";
+        $val = " value=\"" . $funcionario[3] . "\"";
+      }
+      ?>
+    <input class="inp_class" type="time" name="fun_horario" size="28" <?=$dis . $val; ?>><br>
   </div>
   <div class="group-form group-form-cadastro">
     <label class="lbl_class">Inscrição:</label>
-    <input class="inp_class" type="text" name="fun_inscricao" size="28"><br>
+    <?php
+      if ($_SESSION['form'] == 1) {
+        $dis = "";
+        $val = "";
+      } else {
+        $dis = " disabled";
+        $val = " value=\"" . $funcionario[4] . "\"";
+      }
+    ?>
+    <input class="inp_class" type="text" name="fun_inscricao" size="28" <?=$dis . $val; ?>><br>
   </div>
   <div class="group-form group-form-cadastro">
     <label class="lbl_class">Turno:</label>
-    <select class="select" name="fun_turno">
-     <option class="option" value="manha">Manhã</option>
-     <option class="option" value="tarde">Tarde</option>
-     <option class="option" value="noite">Noite</option>
-    </select><br>
+    <?php
+      if ($_SESSION['form'] == 1) {
+      echo "<select class=\"select\" name=\"fun_turno\">";
+      echo "<option class=\"option\" value=\"manha\">Manhã</option>";
+      echo "<option class=\"option\" value=\"tarde\">Tarde</option>";
+      echo "<option class=\"option\" value=\"noite\">Noite</option>";
+      echo "</select><br>";
+      } else {
+        echo "<input class=\"inp_class\" type=\"text\" name=\"fun_turno\" size=\"28\"
+            disabled value = " . $funcionario[5] . "><br>";
+      }
+    ?>
   </div>
   <div class="group-form group-form-cadastro">
     <label class="lbl_class">E-mail:</label>
-    <input class="inp_class" type="text" name="usu_email" size="28"><br>
+    <?php
+      if ($_SESSION['form'] == 1) {
+        $dis = "";
+        $val = "";
+      } else {
+        $dis = " disabled";
+        $val = " value=\"" . $usuario[2] . "\"";
+      }
+    ?>
+    <input class="inp_class" type="text" name="usu_email" size="28" <?=$dis . $val; ?>><br>
   </div>
   <div class="group-form group-form-cadastro">
     <label class="lbl_class">Senha:</label>
-    <input class="inp_class" type="password" name="usu_senha" size="28"><br>
+    <?php
+      if ($_SESSION['form'] == 1) {
+        $dis = "";
+        $val = "";
+      } else {
+        $dis = " disabled";
+        $val = " value=\"" . $usuario[2] . "\"";
+      }
+    ?>
+    <input class="inp_class" type="password" name="usu_senha" size="28" <?=$dis . $val; ?>><br>
   </div>
+  <?php
+    if ($_SESSION['form'] == 2 || $_SESSION['form'] == 3) {
+      echo "<input id=\"0\" type=\"button\" value=\"Alterar\">";
+    }
+  ?>
   <input class="inp_class" type="submit" value="Proximo">
 </form>
