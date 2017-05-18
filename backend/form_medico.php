5bef94e385ba4f8d4ec241bc8ba4df0c100401ed
giseleCarvalho <gih.carvalho.fernandes@gmail.com>
2017-05-17 21:14:19 -0300

    Backend: Selecionando dados dos campos
    	- Está sendo selecionado os nomes dos campos na tabela
    	especialização para inserir na value de seu campo de texto
    	da opção conf. final e Update
    
    	modified:   backend/form_medico.php

--- backend/form_medico.php
+++ backend/form_medico.php
@@ -1,40 +1,47 @@
 <?php
 include_once("Sql.class.php");
 $sql = new Sql;
+
 if ($_SESSION['form'] == 1) {
-    $tipo = "cadastro.php?acao=cadastro&passo=4"; 
+    $tipo = "cadastro.php?acao=cadastro&passo=4";
 } elseif ($_SESSION['form'] == 2) {
     $tipo = "cadastro.php?acao=cadastro&passo=7";
 } else {
     $tipo = "cadastro.php?acao=cadastro&passo=3";
 }
 
-$maxPac = "SELECT MAX(pac_id) AS pac_id FROM paciente";
-$idPac = $sql->selecionar($maxPac);
-$selPac = "SELECT * FROM paciente WHERE pac_id='" . $idPac . "';";
-$paciente = $sql->fetch($selPac);
+$maxMed = "SELECT MAX(med_id) AS med_id FROM medico";
+$idMed = $sql->selecionar($maxMed);
+$selMed = "SELECT * FROM medico WHERE med_id='" . $idMed . "';";
+$medico = $sql->fetch($selMed);
 ?>
 <form class="Form" action="<?=$tipo?>" method="post">
-    <h1>Médico</h1>
-    <div class="group-form group-form-cadastro">
-     <label class="lbl_class">CRM:</label>
-     <?php
-     if ($_SESSION['form'] == 1) {
-        $dis = "";
-        $val = "";
-     } else {
-        $dis = "disabled";
-        $val = " value=\"" . $medico[1] . "\"";
-     }
+  <h1>Médico</h1>
+  <div class="group-form group-form-cadastro">
+    <label class="lbl_class">CRM:</label>
+    <?php
+    if ($_SESSION['form'] == 1) {
+      $dis = "";
+      $val = "";
+    } else {
+      $dis = "disabled";
+      $val = " value=\"" . $medico[1] . "\"";
+    }
      ?>
-     <input class="inp_class" type="text" name="med_crm" size="28" <?=$val . $dis ?>><br>
-     <label class="lbl_class">Especialização:</label>
-     <?php
-       if ($_SESSION['form'] == 1) {
-        $sql->selectbox("especializacao");
-       } else {
-        echo "<input class=\"inp_class\" type=\"text\" name=\"med_crm\" size=\"28\" value=" . $esp[1] .  "><br>";
-       }
-     ?>
-    </div>
+    <input class="inp_class" type="text" name="med_crm" size="28" <?=$val . $dis ?>><br>
+    <label class="lbl_class">Especialização:</label>
+    <?php
+    if ($_SESSION['form'] == 1) {
+      $sql->selectbox("especializacao");
+    } else {
+      // Pegando o nome da especialização
+      $sel = "SELECT * FROM medico_has_especializacao WHERE medico_med_id='" . $idMed . "';";
+      $esp = $sql->fetch($sel);
+      $selEsp = "SELECT * FROM especializacao WHERE esp_id='" . $esp[2] . "';";
+      $es = $sql->fetch($selEsp);
+
+      echo "<input class=\"inp_class\" type=\"text\" name=\"esp_nome\" size=\"28\" value=" . $es[2] .  "><br>";
+    }
+    ?>
+  </div>
 </form>
