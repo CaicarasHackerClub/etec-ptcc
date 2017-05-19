cbc3ac04a5b990d78de8866a937b256e17d4be41
giseleCarvalho <gih.carvalho.fernandes@gmail.com>
2017-05-19 20:55:01 -0300

    Backend: Inserindo formulário "enfermeiro"
    	 Foi inserido o formulário enfermeiro e suas verificações.

---
+++ backend/form_complementar.php
@@ -0,0 +1,86 @@
+<?php
+include_once("Sql.class.php");
+$sql = new Sql;
+
+if ($_SESSION['fun_cargo'] == "medico") {
+  if ($_SESSION['form'] == 1) {
+      $tipo = "cadastro.php?acao=cadastro&passo=4";
+  } elseif ($_SESSION['form'] == 2) {
+      $tipo = "cadastro.php?acao=cadastro&passo=7";
+  } else {
+      $tipo = "cadastro.php?acao=cadastro&passo=3";
+  }
+
+  $maxMed = "SELECT MAX(med_id) AS med_id FROM medico";
+  $idMed = $sql->selecionar($maxMed);
+  $selMed = "SELECT * FROM medico WHERE med_id='" . $idMed . "';";
+  $medico = $sql->fetch($selMed);
+  ?>
+  <form class="Form" action="<?=$tipo?>" method="post">
+    <h1>Médico</h1>
+    <div class="group-form group-form-cadastro">
+      <label class="lbl_class">CRM:</label>
+      <?php
+      if ($_SESSION['form'] == 1) {
+        $dis = "";
+        $val = "";
+      } else {
+        $dis = "disabled";
+        $val = " value=\"" . $medico[1] . "\"";
+      }
+       ?>
+      <input class="inp_class" type="text" name="med_crm" size="28" <?=$dis . $val ?>><br>
+      <label class="lbl_class">Especialização:</label>
+      <?php
+      if ($_SESSION['form'] == 1) {
+        $sql->selectbox("especializacao");
+      } else {
+        // Pegando o nome da especialização
+        $sel = "SELECT * FROM medico_has_especializacao WHERE medico_med_id='" . $idMed . "';";
+        $esp = $sql->fetch($sel);
+        $selEsp = "SELECT * FROM especializacao WHERE esp_id='" . $esp[2] . "';";
+        $es = $sql->fetch($selEsp);
+
+        echo "<input class=\"inp_class\" type=\"text\" name=\"esp_nome\" size=\"28\" value=" . $es[2] .  "><br>";
+      }
+      ?>
+    </div>
+    <input class="inp_class submmit" type="submit" value="Confirmar">
+  </form>
+<?php
+} elseif ($_SESSION['fun_cargo'] == "enfermeiro") {
+
+    if ($_SESSION['form'] == 1) {
+        $tipo = "cadastro.php?acao=cadastro&passo=4";
+    } elseif ($_SESSION['form'] == 2) {
+        $tipo = "cadastro.php?acao=cadastro&passo=7";
+    } else {
+        $tipo = "cadastro.php?acao=cadastro&passo=3";
+    }
+
+    $maxFun = "SELECT MAX(fun_id) AS fun_id FROM funcionario";
+    $idFun = $sql->selecionar($maxFun);
+
+    $sel = "SELECT * FROM enfermeiro WHERE funcionario_fun_id='" . $idFun . "';";
+    $reg = $sql->fetch($sel);
+    ?>
+    <form class="Form" action="<?=$tipo?>" method="post">
+      <h1>Enfermeiro</h1>
+      <div class="group-form group-form-cadastro">
+        <label class="lbl_class">Registro:</label>
+        <?php
+        if ($_SESSION['form'] == 1) {
+          $dis = "";
+          $val = "";
+        } else {
+          $dis = "disabled";
+          $val = "value=\"" . $reg[2] . "\"";
+        }
+        ?>
+        <input class="inp_class" type="text" name="enf_registro" size="28" <?=$dis . $val?>><br>
+      </div>
+      <input class="inp_class submmit" type="submit" value="Confirmar">
+    </form>
+<?php
+}
+?>
