3cb868d374c224402f91a90b54ddb1dd13ee6d78
giseleCarvalho <gih.carvalho.fernandes@gmail.com>
2017-05-22 23:34:54 -0300

    Backend: Verificando as permissões e desabilitando funções
    	A verificação se o usuario pode ou não entrar em alguns modulos
    	pelo cargo foi excluida do cadastro.php e inserida aqui, para
    	fazer as validações dos botões.
    
    	modified:   backend/pagina_inicial.php

--- backend/pagina_inicial.php
+++ backend/pagina_inicial.php
@@ -1,3 +1,6 @@
+<?php
+session_start()
+?>
 <!DOCTYPE html>
 <html>
   <head>
@@ -30,25 +33,70 @@
 
     <div class="conteudoCentro">
       <!-- <form class="form" action="sub_modulo.php" method="post"> -->
-      <a class="anchor" id="anchor-triagem" onclick="enviar(1)" href="../backend/sub_modulo.php"><img class="avatars" id="img-triagem" src="../img/avatars/triagem.png"><p class="txt_avatar" id="name-tri">
+      <?php
+        include_once("Sql.class.php");
+        $sql = new Sql;
+
+        $con=$sql->conecta();
+        $selCar="SELECT f.fun_cargo FROM funcionario f INNER JOIN usuario u ON f.fun_id=u.funcionario_id WHERE u.usu_id='" . $_SESSION['id_usu']. "';";
+
+        $res=mysqli_query($con, $selCar) or die("Erro: id funcionario " . mysqli_error($con) . "<br> Query: " . $query);
+
+        $cargo=mysqli_fetch_array($res);
+
+        $_SESSION['tipo']=$cargo['fun_cargo'];
+
+        if ($_SESSION['tipo'] == "triagem") {
+          $dis1 = "";
+        } else {
+          $link1 = "";
+          $dis1 = "disabled";
+
+        }
+      ?>
+      <a class="anchor" id="anchor-triagem" onclick="enviar(1)" href="../backend/sub_modulo.php<?=$dis1;?>"><img class="avatars" id="img-triagem" src="../img/avatars/triagem.png"><p class="txt_avatar" id="name-tri">
       Triagem</p></a>
       <!-- <input type="hidden" name=Valor value="../img/avatars/triagem.png">
       <input type="hidden" name=Nome_pag value="Triagem">
       </form> -->
-
+      <?php
+        if ($_SESSION['tipo'] == "recepcao") {
+          $link2 = "../backend/sub_modulo.php";
+          $dis2 = "";
+        } else {
+          $link2 = "";
+          $dis2 = "disabled";
+        }
+      ?>
       <!-- <form class="form" action="sub_modulo.php" method="post"> -->
-      <a class="anchor" href="../backend/sub_modulo.php" onclick="enviar(2)"><img class="avatars" id="img-recepcao" src="../img/avatars/recepcao.png"><p class="txt_avatar" id="name-recep">Recepção</p></a>
+      <a class="anchor" href="../backend/sub_modulo.php<=?$dis2;?>" onclick="enviar(2)"><img class="avatars" id="img-recepcao" src="../img/avatars/recepcao.png"><p class="txt_avatar" id="name-recep">Recepção</p></a>
       <!-- <input type="hidden" name=Valor value="../img/avatars/recepcao.png">
       <input type="hidden" name=Nome_pag value="Recepcao">
       </form> -->
-
+      <?php
+        if ($_SESSION['tipo'] == "administracao") {
+          $link3 = "../backend/sub_modulo.php";
+          $dis3 = "";
+        } else {
+          $link3 = "";
+          $dis3 = "disabled";
+        }
+      ?>
       <!-- <form class="form" action="sub_modulo.php" method="post"> -->
-      <a class="anchor" href="../backend/sub_modulo.php" onclick="enviar(3)"><img class="avatars" id="img-admin" src="../img/avatars/adm.png"><p class="txt_avatar" id="name-admin">Administração</p></a>
+      <a class="anchor" href= "../backend/sub_modulo.php <?=$dis3?>" onclick="enviar(3)"><img class="avatars" id="img-admin" src="../img/avatars/adm.png"><p class="txt_avatar" id="name-admin">Administração</p></a>
       <!-- <input type="hidden" name=Valor value="../img/avatars/adm.png">
       <input type="hidden" name=Nome_pag value="Administracao">
       </form> -->
-
-      <a class="anchor" href="../backend/sub_modulo.php" onclick="enviar(4)"><img class="avatars" id="img-portaria" src="../img/avatars/portaria.png"><p class="txt_avatar" id="name-portaria">Portaria</p></a>
+      <?php
+        if ($_SESSION['tipo'] == "portaria") {
+          $link4 = "../backend/sub_modulo.php";
+          $dis4 = "";
+        } else {
+          $link4 = "";
+          $dis4 = "disabled";
+        }
+      ?>
+      <a class="anchor" href="../backend/sub_modulo.php <?=$dis4?>" onclick="enviar(4)"><img class="avatars" id="img-portaria" src="../img/avatars/portaria.png"><p class="txt_avatar" id="name-portaria">Portaria</p></a>
 
     </div>
 
