72aa46beeffbae0034cd56be5bfc744a7b963a85
Jonatas Ramos <jo962011@gmail.com>
2017-05-31 19:33:44 -0300

    FrontEnd: Identado os links para o sub-módulo.

--- backend/pagina_inicial.php
+++ backend/pagina_inicial.php
@@ -16,91 +16,101 @@ session_start()
 
   <div class="container">
     <nav class="lateral" id="lateral">
-    <img class="logo" src="../img/logos/index.png">
-    <a href="#" id="user" class="nav_user user">Usuário</a>
-    <a href="../index.php?acao=logoff" class="nav_user" >Deslogar</a>
-    <div class="hora" id="hora"></div>
+      <img class="logo" src="../img/logos/index.png">
+      <a href="#" id="user" class="nav_user user">Usuário</a>
+      <a href="../index.php?acao=logoff" class="nav_user" >Deslogar</a>
+      <div class="hora" id="hora"></div>
     </nav>
 
     <div class="main">
 
-    <div class="cabecalho">
-      <header>
-      <img src="../img/icons/botao_menu.png" class="btn_slide" onclick="mostrarMenu()" >
-      </header>
-      <h1 class="titulo">Hug & Health</h1>
-    </div>
+      <div class="cabecalho">
+        <header>
+        <img src="../img/icons/botao_menu.png" class="btn_slide" onclick="mostrarMenu()" >
+        </header>
+        <h1 class="titulo">Hug & Health</h1>
+      </div>
 
-    <div class="conteudoCentro">
-      <!-- <form class="form" action="sub_modulo.php" method="post"> -->
-      <?php
-        echo $_SESSION['id_usu'];
-        include_once("Sql.class.php");
-        $sql = new Sql;
+      <div class="conteudoCentro">
+        <!-- <form class="form" action="sub_modulo.php" method="post"> -->
+        <?php
+          echo $_SESSION['id_usu'];
+          include_once("Sql.class.php");
+          $sql = new Sql;
 
-        $con=$sql->conecta();
-        $selCar="SELECT f.fun_cargo FROM funcionario f INNER JOIN usuario u ON f.fun_id=u.funcionario_id WHERE u.usu_id=" . $_SESSION['id_usu']. ";";
+          $con=$sql->conecta();
+          $selCar="SELECT f.fun_cargo FROM funcionario f INNER JOIN usuario u ON f.fun_id=u.funcionario_id WHERE u.usu_id=" . $_SESSION['id_usu']. ";";
 
-        $res=mysqli_query($con, $selCar) or die("Erro: id funcionario " . mysqli_error($con) . "<br> Query: " . $query);
+          $res=mysqli_query($con, $selCar) or die("Erro: id funcionario " . mysqli_error($con) . "<br> Query: " . $query);
 
-        $cargo=mysqli_fetch_array($res);
+          $cargo=mysqli_fetch_array($res);
 
-        $_SESSION['tipo']=$cargo['fun_cargo'];
+          $_SESSION['tipo']=$cargo['fun_cargo'];
 
-        if ($_SESSION['tipo'] == "enfermeiro" || $_SESSION['tipo'] == "medico") {
-          $link1 = "../backend/sub_modulo.php";
+          if ($_SESSION['tipo'] == "enfermeiro" || $_SESSION['tipo'] == "medico") {
+            $link1 = "../backend/sub_modulo.php";
 
-        } else {
-          $link1 = "";
+          } else {
+            $link1 = "";
 
 
-        }
-      ?>
-      <a class="anchor" id="anchor-triagem" onclick="enviar(1)" href="<?=$link1?>"><img class="avatars" id="img-triagem" src="../img/avatars/triagem.png"><p class="txt_avatar" id="name-tri">
-      Triagem</p></a>
-      <!-- <input type="hidden" name=Valor value="../img/avatars/triagem.png">
-      <input type="hidden" name=Nome_pag value="Triagem">
-      </form> -->
-      <?php
-        if ($_SESSION['tipo'] == "recepcao") {
-          $link2 = "../backend/sub_modulo.php";
+          }
+        ?>
+        <a class="anchor" id="anchor-triagem" onclick="enviar(1)" href="<?=$link1?>">
+          <img class="avatars" id="img-triagem" src="../img/avatars/triagem.png">
+          <p class="txt_avatar" id="name-tri">Triagem</p>
+        </a>
+        <!-- <input type="hidden" name=Valor value="../img/avatars/triagem.png">
+        <input type="hidden" name=Nome_pag value="Triagem">
+        </form> -->
+        <?php
+          if ($_SESSION['tipo'] == "recepcao") {
+            $link2 = "../backend/sub_modulo.php";
 
-        } else {
-          $link2 = "";
+          } else {
+            $link2 = "";
 
-        }
-      ?>
-      <!-- <form class="form" action="sub_modulo.php" method="post"> -->
-      <a class="anchor" href="<?=$link2?>" onclick="enviar(2)"><img class="avatars" id="img-recepcao" src="../img/avatars/recepcao.png"><p class="txt_avatar" id="name-recep">Recepção</p></a>
-      <!-- <input type="hidden" name=Valor value="../img/avatars/recepcao.png">
-      <input type="hidden" name=Nome_pag value="Recepcao">
-      </form> -->
-      <?php
-        if ($_SESSION['tipo'] == "administracao") {
-          $link3 = "../backend/sub_modulo.php";
+          }
+        ?>
+        <!-- <form class="form" action="sub_modulo.php" method="post"> -->
+        <a class="anchor" href="<?=$link2?>" onclick="enviar(2)">
+          <img class="avatars" id="img-recepcao" src="../img/avatars/recepcao.png">
+          <p class="txt_avatar" id="name-recep">Recepção</p>
+        </a>
+        <!-- <input type="hidden" name=Valor value="../img/avatars/recepcao.png">
+        <input type="hidden" name=Nome_pag value="Recepcao">
+        </form> -->
+        <?php
+          if ($_SESSION['tipo'] == "administracao") {
+            $link3 = "../backend/sub_modulo.php";
 
-        } else {
-          $link3 = "";
+          } else {
+            $link3 = "";
 
-        }
-      ?>
-      <!-- <form class="form" action="sub_modulo.php" method="post"> -->
-      <a class="anchor" href= "<?=$link3?>" onclick="enviar(3)"><img class="avatars" id="img-admin" src="../img/avatars/adm.png"><p class="txt_avatar" id="name-admin">Administração</p></a>
-      <!-- <input type="hidden" name=Valor value="../img/avatars/adm.png">
-      <input type="hidden" name=Nome_pag value="Administracao">
-      </form> -->
-      <?php
-        if ($_SESSION['tipo'] == "portaria") {
-          $link4 = "../backend/sub_modulo.php";
+          }
+        ?>
+        <!-- <form class="form" action="sub_modulo.php" method="post"> -->
+        <a class="anchor" href= "<?=$link3?>" onclick="enviar(3)">
+          <img class="avatars" id="img-admin" src="../img/avatars/adm.png">
+          <p class="txt_avatar" id="name-admin">Administração</p>
+        </a>
+        <!-- <input type="hidden" name=Valor value="../img/avatars/adm.png">
+        <input type="hidden" name=Nome_pag value="Administracao">
+        </form> -->
+        <?php
+          if ($_SESSION['tipo'] == "portaria") {
+            $link4 = "../backend/sub_modulo.php";
 
-        } else {
-          $link4 = "";
+          } else {
+            $link4 = "";
 
-        }
-      ?>
-      <a class="anchor" href="<?=$link4?>" onclick="enviar(4)"><img class="avatars" id="img-portaria" src="../img/avatars/portaria.png"><p class="txt_avatar" id="name-portaria">Portaria</p></a>
-
-    </div>
+          }
+        ?>
+        <a class="anchor" href="<?=$link4?>" onclick="enviar(4)">
+          <img class="avatars" id="img-portaria" src="../img/avatars/portaria.png">
+          <p class="txt_avatar" id="name-portaria">Portaria</p>
+        </a>
+      </div>
 
     </div>
   </div>
